619a89d49cd818261b17723b87792856da37df89
Hafsa H <hafsasiddiqui94@gmail.com>
2024-10-31 19:37:12 +0500

    🔎 added search filter component with shadcn

---
+++ src/components/SearchFilter.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+export default function SearchFilter() {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = () => {
+    console.log("Search query: ", query);
+  };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+  return (
+    <div className="flex justify-center w-full ">
+      <Input
+        className="w-48 h-10"
+        type="text"
+        placeholder="Search here"
+        value={query}
+        onChange={handleChange}
+      />
+      <Button type="submit" onClick={handleSearch}>
+        Search
+      </Button>
+    </div>
+  );
+}
